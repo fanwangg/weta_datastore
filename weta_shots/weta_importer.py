dfac06343c9e8db937d473098f8d362466b644f9
Fan Wang <fanwangg@gmail.com>
2019-02-03 17:42:04 +0800

    Implement ShotGroup class for aggregation

--- weta_shots/weta_importer.py
+++ weta_shots/weta_importer.py
@@ -1,6 +1,7 @@
 import csv
 import pickle
 
+from enum import Enum
 from typing import NamedTuple
 
 
@@ -15,7 +16,7 @@ class Shot(NamedTuple):
 
     @property
     def uid(self):
-        return (self.project, self.shot, self.version)
+        return (self.PROJECT, self.SHOT, self.VERSION)
 
     @property
     def PROJECT(self):
@@ -44,6 +45,71 @@ class Shot(NamedTuple):
     @property
     def CREATED_DATE(self):
         return self.created_date
+
+
+class Aggregation(Enum):
+    MIN = ('min', lambda s: min(s))
+    MAX = ('max', lambda s: max(s))
+    SUM = ('sum', lambda s: sum(s))
+    COUNT = ('count', lambda s: len(s))
+    COLLECT = ('collect', lambda s: s)
+
+    def __call__(self, *args, **kwargs):
+        return self.value[1](*args, **kwargs)
+
+    @classmethod
+    def from_string(cls, s):
+        for c in cls:
+            if c.value[0] == s:
+                return c
+        raise ValueError(cls.__name__ + ' has no value matching "{s}"')
+
+
+class ShotGroup:
+    def __init__(self, aggregrations: dict):
+        self.shots = list()
+        self.aggregating_method = dict()
+
+        for k, v in aggregrations.items():
+            self.aggregating_method[k] = Aggregation.from_string(v)
+
+    def add_shots(self, shots):
+        self.shots.extend(shots)
+
+    def get_properity(self, property):
+        return [getattr(s, property) for s in self.shots]
+
+    def get_aggregated_property(self, property):
+        aggregating_method = self.aggregating_method.get(property, Aggregation.COLLECT)
+        return aggregating_method(self.get_properity(property))
+
+    @property
+    def PROJECT(self):
+        return self.get_aggregated_property('PROJECT')
+
+    @property
+    def SHOT(self):
+        return self.get_aggregated_property('SHOT')
+
+    @property
+    def VERSION(self):
+        return self.get_aggregated_property('VERSION')
+
+    @property
+    def STATUS(self):
+        return self.get_aggregated_property('STATUS')
+
+    @property
+    def FINISH_DATE(self):
+        return self.get_aggregated_property('FINISH_DATE')
+
+    @property
+    def INTERNAL_BID(self):
+        return self.get_aggregated_property('INTERNAL_BID')
+
+    @property
+    def CREATED_DATE(self):
+        return self.get_aggregated_property('CREATED_DATE')
 
 
 def import_from_file(filename):
