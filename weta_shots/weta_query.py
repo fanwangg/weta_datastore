2d8d84e2f7e9ade36db17733729d8a822926ddd3
Fan Wang <fanwangg@gmail.com>
2019-02-04 06:18:07 +0800

    Pack querier as class

--- weta_shots/weta_query.py
+++ weta_shots/weta_query.py
@@ -5,92 +5,92 @@ from weta_importer import WetaImporter
 from weta_shot import Shot, ShotGroup
 
 
-def filter_multiple(all_data, args):
-    def operate(op, second, first):
-        if op == 'AND':
-            return first & second
-        elif op == 'OR':
-            return first | second
-        else:
-            print(f'Wrong op {op}')
+class WetaQuery():
+    def __init__(self, data):
+        self.data = data
+        self.selected_data = None
+        self.selection_method = None
 
-    def precedence(current_op, op_from_ops):
-        if op_from_ops == '(' or op_from_ops == ')':
-            return False
-        elif (current_op == 'OR') and (op_from_ops == 'AND'):
-            return False
-        else:
-            return True
+    def filter_multiple(self, args):
+        def operate(op, second, first):
+            if op == 'AND':
+                return first & second
+            elif op == 'OR':
+                return first | second
+            else:
+                print(f'Wrong op {op}')
 
-    parsed_args = re.split(r'''(\w+\s*=\"[\w\s]+\")|(\w+\s*=[\w]+)|(AND|and)|(OR|or)|([()]+)''', args)
-    parsed_args = [pa for pa in parsed_args if pa and pa != ' ']
-    data_stack, ops_stack = [], []
+        def precedence(current_op, op_from_ops):
+            if op_from_ops == '(' or op_from_ops == ')':
+                return False
+            elif (current_op == 'OR') and (op_from_ops == 'AND'):
+                return False
+            else:
+                return True
 
-    for parsed_arg in parsed_args:
-        if parsed_arg == '(':
-            ops_stack.append('(')
-        elif parsed_arg == ')':
-            data_stack.append()
-            while ops_stack[-1] != '(':
-                data_stack.append(operate(ops_stack.pop(), data_stack.pop(), data_stack.pop()))
-            ops_stack.pop()
-        elif parsed_arg in ['AND', 'OR']:
-            while len(ops_stack) != 0 and precedence(parsed_arg, ops_stack[-1]):
-                data_stack.append(operate(ops_stack.pop(), data_stack.pop(), data_stack.pop()))
-            ops_stack.append(parsed_arg)
-        else:
-            data_stack.append(set(filter_single(all_data, parsed_arg)))
+        parsed_args = re.split(r'''(\w+\s*=\"[\w|\s|-]+\")|(\w+\s*=[\w|-]+)|(AND|and)|(OR|or)|([()]+)''', args)
+        parsed_args = [pa for pa in parsed_args if pa and pa != ' ']
+        data_stack, ops_stack = [], []
 
-    while len(ops_stack) > 0:
-        data_stack.append(operate(ops_stack.pop(), data_stack.pop(), data_stack.pop()))
+        for parsed_arg in parsed_args:
+            if parsed_arg == '(':
+                ops_stack.append('(')
+            elif parsed_arg == ')':
+                data_stack.append()
+                while ops_stack[-1] != '(':
+                    data_stack.append(operate(ops_stack.pop(), data_stack.pop(), data_stack.pop()))
+                ops_stack.pop()
+            elif parsed_arg in ['AND', 'OR']:
+                while len(ops_stack) != 0 and precedence(parsed_arg, ops_stack[-1]):
+                    data_stack.append(operate(ops_stack.pop(), data_stack.pop(), data_stack.pop()))
+                ops_stack.append(parsed_arg)
+            else:
+                data_stack.append(set(self.filter_single(self.data, parsed_arg)))
 
-    return list(data_stack.pop())
+        while len(ops_stack) > 0:
+            data_stack.append(operate(ops_stack.pop(), data_stack.pop(), data_stack.pop()))
 
+        self.data = list(data_stack.pop())
 
-def filter_single(data, arg_str):
-    arg_str = arg_str.replace('"', '')
-    parsed_args = re.search(r'''(\w+)='*([\w|\s|-]+)'*''', arg_str)
-    data = list(filter(lambda d: getattr(d, parsed_args[1]) == parsed_args[2], data))
-    return data
+    def filter_single(self, data, arg_str):
+        arg_str = arg_str.replace('"', '')
+        parsed_args = re.search(r'''(\w+)='*([\w|\s|-]+)'*''', arg_str)
+        data = list(filter(lambda d: getattr(d, parsed_args[1]) == parsed_args[2], data))
+        return data
 
+    def aggregate(self, aggregate_keys):
+        groups = defaultdict(list)
+        for d in self.data:
+            group_index = tuple(getattr(d, k) for k in aggregate_keys)
+            groups[group_index].append(d)
 
-def aggregate_data(data, aggregate_keys, aggregate_methods):
-    groups = defaultdict(list)
-    for d in data:
-        group_index = tuple(getattr(d, k) for k in aggregate_keys)
-        groups[group_index].append(d)
+        aggregated_data = []
+        for group in groups.values():
+            shot_group = ShotGroup(shots=group, aggregrations=self.selection_method)
+            aggregated_data.append(shot_group)
 
-    aggregated_data = []
-    for group in groups.values():
-        shot_group = ShotGroup(shots=group, aggregrations=aggregate_methods)
-        aggregated_data.append(shot_group)
+        self.data = aggregated_data
 
-    return aggregated_data
+    def select(self):
+        columns = self.selection_method.keys()
+        rows = [[str(getattr(d, col)) for col in columns] for d in self.data]
+        self.selected_data = [','.join(row) for row in rows]
 
+    def sort(self, sorting_keys):
+        self.data.sort(key=lambda s: tuple(getattr(s, k) for k in sorting_keys))
 
-def select_column(data, columns):
-    rows = [[str(getattr(d, col)) for col in columns] for d in data]
-    return [','.join(row) for row in rows]
+    def output(self):
+        for d in self.selected_data:
+            print(d)
 
+    def set_selection_method(self, selections):
+        selections_with_aggregation = dict()
 
-def sort_by_order(data, sorting_keys):
-    data.sort(key=lambda s: tuple(getattr(s, k) for k in sorting_keys))
-    return data
+        for sel in selections:
+            parsed_sel = sel.split(':')
+            selections_with_aggregation[parsed_sel[0]] = parsed_sel[1] if len(parsed_sel) > 1 else None
 
-
-def output(data):
-    for d in data:
-        print(d)
-
-
-def parse_selection(selections):
-    selections_with_aggregation = dict()
-
-    for sel in selections:
-        parsed_sel = sel.split(':')
-        selections_with_aggregation[parsed_sel[0]] = parsed_sel[1] if len(parsed_sel) > 1 else None
-
-    return selections_with_aggregation
+        self.selection_method = selections_with_aggregation
 
 
 def main():
@@ -107,25 +107,23 @@ def main():
         print(args)
 
     data = WetaImporter.pickle_load('../output.pkl')
+    querier = WetaQuery(data)
 
     if args.filter:
-        data = filter_multiple(data, args.filter)
+        querier.filter_multiple(args.filter)
 
     if args.order:
         sorting_keys = args.order.split(',')
-        data = sort_by_order(data, sorting_keys=sorting_keys)
+        querier.sort(sorting_keys=sorting_keys)
 
-    selected_method = parse_selection(args.select.split(','))
+    querier.set_selection_method(args.select.split(','))
 
     if args.group:
         aggregate_keys = args.group.split(',')
-        data = aggregate_data(data,
-                              aggregate_keys=aggregate_keys,
-                              aggregate_methods=selected_method)
+        querier.aggregate(aggregate_keys=aggregate_keys)
 
-    selected_data = select_column(data, columns=selected_method.keys())
-
-    output(selected_data)
+    querier.select()
+    querier.output()
 
 
 if __name__ == '__main__':
