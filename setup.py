34ec9853e12d359a6bfbb765b4b9b273da54092c
Fan Wang <fanwangg@gmail.com>
2019-02-04 08:29:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='weta_importer',
-      version='1.0',
-      packages=['weta_datastore'],
+setup(name='weta_datastore',
+      version='0.1',
+      author='Fan Wang',
+      author_email='fanwangg@gmail.com',
+      packages=find_packages(),
       entry_points={
-              'console_scripts': ['weta-import=weta_datastore.weta_import:main',
-                                  'weta-import=weta_datastore.weta_query:main'],
+              'console_scripts': ['weta_import=weta_datastore.weta_import:main',
+                                  'weta_query=weta_datastore.weta_query:main'],
           },
       )
